c9148d5988a9fb36bbaca5dd8db54de7ab6a1140
Ferlony <Hegecioum@gmail.com>
2023-01-24 03:30:46 +0500

    add kill fun

--- telpicturesbot/main.py
+++ telpicturesbot/main.py
@@ -12,27 +12,24 @@ logging.basicConfig(
 
 
 def menus(bot_tele: BotTele.BotTele):
-    bot_started_flag = False
+    bot_polling_started_flag = False
     while True:
         print("Choose action\n"
-              "'1' Start bot\n"
-              "'2' Stop bot\n"
-              "'3' Get bot info\n"
-              "'4' Get user update info\n"
+              "'1' Start bot polling\n"
+              "'2' Get bot info\n"
+              "'3' Get user update info\n"
+              "'4' Send message to user\n"
               "'0' Close program")
         inp = input()
         if inp == "1":
-            bot_tele.start_polling()  # TODO: make multiprocess
-            bot_started_flag = True
-        elif inp == "2":
-            if bot_started_flag:
-                pass  # TODO: stop fun
-            else:
-                print("Bot has not been started")
+            bot_tele.start_polling()
         elif inp == "3":
             print(asyncio.run(bot_tele.get_bot_info()))
         elif inp == "4":
             print(asyncio.run(bot_tele.get_update_info()))  # Should send message to the bot
+        elif inp == "5":
+            message = input("Input message")
+            print(asyncio.run(bot_tele.send_message_to_chat_id(message)))
         elif inp == "0":
             print("Closing program")
             break
@@ -43,11 +40,12 @@ def menus(bot_tele: BotTele.BotTele):
 def main():
     config = configparser.ConfigParser()
     config.read("local" + sep + "config.ini")
-    USER_ID = config["SECRETS"]["USER_ID"]
     TOKEN = config["SECRETS"]["TOKEN"]
+    USER_ID = config["SECRETS"]["USER_ID"]
+    CHAT_ID = config["SECRETS"]["CHAT_ID"]
     PRIVATE = bool(config["DEFAULT"]["PRIVATE"])
 
-    menus(BotTele.BotTele(TOKEN, USER_ID, PRIVATE))
+    menus(BotTele.BotTele(TOKEN, USER_ID, CHAT_ID, PRIVATE))
 
 
 if __name__ == "__main__":
